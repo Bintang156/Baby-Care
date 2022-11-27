cafcff7b80b119bca879ad032e2f1ab6feb289a5
Arif Chandra Firmansyah <Sa354127@gmail.com>
2022-11-27 11:49:11 +0700

    create folder main react baby-care apps

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './styles/style.css'
+import './styles/responsive.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Welcome To Baby-Care Apps Development Rooms
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
